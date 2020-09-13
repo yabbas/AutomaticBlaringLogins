e898a4ff839c21b8bee88b213524b0c17444a10a
Yasin Abbas <yabbas@gmail.com>
2020-09-13 20:41:43 +0000

    settup

--- index.java
+++ index.java
@@ -6,8 +6,8 @@ void draw() {
 	background(0);
 	noStroke();
 	
-	for(int x = 0; x < 100; x++)
-		for(int y = 0; y < 100; y++) {
+	for(int x = 0; x < 120; x++)
+		for(int y = 0; y < 120; y++) {
 			fill(lerpColor(color(0, mouseX/2, mouseY/2), color(mouseX/2, y*2, mouseY/2), x/100));
 			rect(x * 5, y * 5, 5, 5);
 		}
